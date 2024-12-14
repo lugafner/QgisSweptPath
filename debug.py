d336fad47b9d9bdeaad0b01ab50645a94dbfb148
lugafner <lu.gafner@outlook.com>
2024-12-14 15:33:42 +0100

    Testing

--- debug.py
+++ debug.py
@@ -2,10 +2,33 @@ from vehicle import Vehicle
 from coord import CartesianCoord
 import math
 
-v = Vehicle()
+v_parent = Vehicle()
+v_trailer = Vehicle()
 
-azi = 0
+v_parent._trailer = v_trailer
 
-v.place_vehicle(CartesianCoord(0, 0), azi / 180 * math.pi)
 
-print(v._global_h)
+azi = 0.0
+steer = 45.0
+
+v_parent.place_vehicle(CartesianCoord(0.0, 0.0), azi / 180 * math.pi)
+print(str(v_parent._global_h.x) + "," + 
+      str(v_parent._global_h.y) + "," +
+      str(v_parent._global_f.x) + "," +
+      str(v_parent._global_f.y) + "," +
+      str(v_trailer._global_h.x) + "," + 
+      str(v_trailer._global_h.y) + "," +
+      str(v_trailer._global_f.x) + "," +
+      str(v_trailer._global_f.y))
+
+for i in range(40):
+    v_parent.step(steer, 0.5)
+    print(str(v_parent._global_h.x) + "," + 
+        str(v_parent._global_h.y) + "," +
+        str(v_parent._global_f.x) + "," +
+        str(v_parent._global_f.y) + "," +
+        str(v_trailer._global_h.x) + "," + 
+        str(v_trailer._global_h.y) + "," +
+        str(v_trailer._global_f.x) + "," +
+        str(v_trailer._global_f.y))
+
