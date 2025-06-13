5c529778875b2c1ee27337f6a5468f53fe0a4ea3
Lukas Gafner <179678506+lugafner@users.noreply.github.com>
2025-06-13 21:43:24 +0200

    Implement vehicle placer

--- vehicle_placer.py
+++ vehicle_placer.py
@@ -1,15 +1,56 @@
+from networkx.classes import selfloop_edges
 from qgis.gui import QgsMapToolEmitPoint, QgisInterface, QgsMapCanvas, QgsMapMouseEvent
+from qgis.core import QgsPointXY
+from qgis.PyQt.QtCore import Qt
+
+from .coord import CoordUtils, PolarCoord, CartesianCoord
+from .vehicle import Vehicle
 
 
 class VehiclePlacer(QgsMapToolEmitPoint):
-    def __init__(self, iface: QgisInterface):
-        self.iface: QgisInterface = iface
-        self.canvas: QgsMapCanvas = iface.mapCanvas()
-        QgsMapToolEmitPoint.__init__(self, self.canvas)
+    def __init__(self, iface: QgisInterface, vehicle: Vehicle):
+        self._iface: QgisInterface = iface
+        self._canvas: QgsMapCanvas = iface.mapCanvas()
+        self._vehicle: Vehicle = vehicle
+
+        # First and second point
+        self._base_point: QgsPointXY = QgsPointXY(0.0, 0.0)
+        self._rotation_point: QgsPointXY = QgsPointXY(1.0, 0.0)
+        self._click_counter = 0
+
+        QgsMapToolEmitPoint.__init__(self, self._canvas)
+
 
     def canvasPressEvent(self, e: QgsMapMouseEvent):
-        self._clicked_point = self.toMapCoordinates(e.pos())
+        if e.button() == Qt.LeftButton:
+            self._set_clicked_coordinate(self.toMapCoordinates(e.pos()))
+            print("left")
 
-    @property
-    def clicked_point(self):
-        return self._clicked_point
+        if e.button() == Qt.RightButton:
+            self._place()
+            print("right")
+
+
+    def _set_clicked_coordinate(self, position: QgsPointXY):
+        if self._click_counter == 0:
+            # Set base point coordinate
+            self._base_point = position
+            self._click_counter += 1
+        else:
+            # Set rotation point coordinate
+            self._rotation_point = position
+            self._click_counter = 0
+
+
+    def _place(self):
+        rotation_polar: PolarCoord = CoordUtils.to_polar(
+            dx=self._rotation_point.x() - self._base_point.x(),
+            dy=self._rotation_point.y() - self._base_point.y()
+        )
+        position_cartesian: CartesianCoord = CartesianCoord(
+            x=self._base_point.x(),
+            y=self._base_point.y()
+        )
+
+        self._vehicle.place_vehicle(position_cartesian, rotation_polar.a)
+        self.deactivate()
